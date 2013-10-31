8534a18ca93be0057cec77c4796b79f28c399275
DownSpiral <eric.d.wishart@gmail.com>
2013-10-31 14:02:09 -0700

    Add non-determinism to get_move
    * Adds a 20% chance to return the opposite move from get_move.

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 # valid moves
+import random
 RAT_OUT = 0
 STAY_SILENT = 1
 
@@ -13,3 +14,9 @@ results = {
 
 def solve(move1, move2):
     return results[(move1, move2)]
+
+def opposite_move(move):
+    return (STAY_SILENT if move == RAT_OUT else  RAT_OUT)
+
+def communication_failed():
+    return random.randint(0, 4) == 0
