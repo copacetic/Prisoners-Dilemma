25ba5358ce2d803fbc4466c1f61eadec30a53c4d
Zachary Bush <Zachary.bush@meraki.net>
2013-10-31 13:36:15 -0700

    Fix main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import TourneyMaster
+import os
+
 MATCH_SYSTEM = "round robin"
 ROUND_SYSTEM = "random"
-PLAYER_DIRECTORY = '/home/hayg/Players'
+PLAYER_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Players')
 
 print "The tournament will be: ", MATCH_SYSTEM
 tourney = TourneyMaster.TourneyMaster(_tournamentType=MATCH_SYSTEM, _roundSystem=ROUND_SYSTEM)
 tourney.load_player_modules(PLAYER_DIRECTORY)
 print "Ladies and gentleman, today's contestants are: ", tourney.get_player_names()
 tourney.create_matches()
-print "The matches are in! The matches are as follows: ", tourney.get_matches() 
+print "The matches are in! The matches are as follows: ", tourney.get_matches()
 tourney.start_tourney()
 winner = tourney.get_winner()
 print "The final score is: ", tourney.get_score()
